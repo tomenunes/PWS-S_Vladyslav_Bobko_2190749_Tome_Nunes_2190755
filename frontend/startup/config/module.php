5f468756fa54f513a606a2b80387881e419f6b8a
Tomé Nunes <2190755@my.ipleiria.pt>
2021-05-26 15:30:39 +0100

    frontend

--- frontend/startup/config/module.php
+++ frontend/startup/config/module.php
@@ -5,7 +5,7 @@
 // registerServerMod
 
 define('WL_SERVER_URL',                         'http://localhost/');
-define('WL_SERVER_MOD_NAME',                    'webapp');
+define('WL_SERVER_MOD_NAME',                    'frontend');
 define('WL_SERVER_PUBLIC_DIR_NAME',             'public');
 define('WL_RUNNING_ENV',                        'dev');
 define('WL_DEBUG_ENABLED',                      true);
